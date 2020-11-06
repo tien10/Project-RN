585e3400a8e75679cff75471f1a46626f2e7ada2
tien10 <nguyentien7566@gmail.com>
2020-11-06 16:31:05 +0700

    error npm start

---
+++ src/screens/shop/ProductDetailScreen.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { StyleSheet, Text } from "react-native";
+const ProductDetailScreen = (props) => {
+  // console.log(props.navigation.getParam("productId"));
+  // console.log(props.navigation);
+  return <Text>ProductDetailScreen</Text>;
+};
+const styles = StyleSheet.create({});
+export default ProductDetailScreen;
--- src/screens/shop/ProductsOverviewScreen.js
+++ src/screens/shop/ProductsOverviewScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { FlatList, Text } from "react-native";
 import { useSelector } from "react-redux";
+import ProductItem from "../../components/shop/ProductItem";
 
 const ProductsOverviewScreen = (props) => {
   const products = useSelector((state) => state.products.availableProducts);
@@ -8,7 +9,22 @@ const ProductsOverviewScreen = (props) => {
     <FlatList
       data={products}
       keyExtractor={(item) => item.id}
-      renderItem={(itemData) => <Text>{itemData.item.title}</Text>}
+      renderItem={(itemData) => (
+        <ProductItem
+          image={itemData.item.imageUrl}
+          title={itemData.item.title}
+          price={itemData.item.price}
+          onViewDetail={() => {
+            console.log("onViewDetail");
+            props.navigation.navigate("ProductDetail", {
+              productId: itemData.item.id,
+            });
+          }}
+          onAddToCart={() => {
+            console.log("onAddToCart");
+          }}
+        />
+      )}
     />
   );
 };
