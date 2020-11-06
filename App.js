971086bd207aa426dbefdc0b6f02a48afe6c0c24
tien10 <nguyentien7566@gmail.com>
2020-11-06 09:22:45 +0700

    build ra file apk

--- App.js
+++ App.js
@@ -1,11 +1,44 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useEffect } from "react";
+import { Button, StyleSheet, Text, View } from "react-native";
+import * as Notifications from "expo-notifications";
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
 
 export default function App() {
+  useEffect(() => {
+    const thongBaoNen = Notifications.addNotificationResponseReceivedListener(
+      (res) => console.log(res)
+    );
+    const thongBao = Notifications.addNotificationReceivedListener((tb) =>
+      console.log(tb)
+    );
+    return () => {
+      thongBao.remove();
+      thongBaoNen.remove();
+    };
+  }, []);
+  const showNotification = () => {
+    console.log("showNotification");
+    Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Notification",
+        body: "Notification 01",
+      },
+      trigger: {
+        seconds: 5,
+      },
+    });
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Show Notification" onPress={showNotification} />
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +47,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
